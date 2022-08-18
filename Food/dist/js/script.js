87d2c96bee2e844d27439763444871e6972a7423
Peter Grebenek <petergrebenek@gmail.com>
2022-08-18 09:37:29 +0300

    add promo timer for project Food

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -94,6 +94,7 @@
 /***/ (function(module, exports) {
 
 window.addEventListener("DOMContentLoaded", () => {
+  //####################### СОЗДАНИЕ ТАБОВ ############################
   const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
@@ -129,7 +130,69 @@ window.addEventListener("DOMContentLoaded", () => {
         }
       });
     }
-  });
+  }); //####################### СОЗДАНИЕ ТАЙМЕРА ############################
+
+  const deadline = "2020-08-19";
+
+  function getTimeReamaining(endTime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endTime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      hours = Math.floor(t / (1000 * 60 * 60) % 24);
+      minutes = Math.floor(t / 1000 / 60 % 60);
+      seconds = Math.floor(t / 1000 % 60);
+    }
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endTime) {
+    // Данные со страницы html
+    const timer = document.querySelector(selector),
+          days = timer.querySelector("#days"),
+          hours = timer.querySelector("#hours"),
+          minutes = timer.querySelector("#minutes"),
+          seconds = timer.querySelector("#seconds"),
+          timerInterval = setInterval(updateClock, 1000);
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeReamaining(endTime); // Вписываем данные на страницу
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        // Остановка обновления таймера
+        clearInterval(timerInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
 });
 
 /***/ })

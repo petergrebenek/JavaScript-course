6b2daa188e76ff08399ea93a13487c1f7fca4acf
Peter Grebenek <petergrebenek@gmail.com>
2022-08-21 10:10:09 +0300

    use class for cards

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -204,8 +204,7 @@ window.addEventListener("DOMContentLoaded", () => {
     modal.classList.remove("hide"); // modal.classList.toggle("show");
 
     document.body.style.overflow = "hidden"; //отключить скрол
-
-    clearInterval(modalTimerId);
+    // clearInterval(modalTimerId);
   }
 
   modalTrigger.forEach(btn => {
@@ -229,8 +228,7 @@ window.addEventListener("DOMContentLoaded", () => {
     if (e.code =  true && modal.classList.contains("show")) {
       closeModal();
     }
-  });
-  const modalTimerId = setTimeout(openModal, 5000);
+  }); // const modalTimerId = setTimeout(openModal, 5000);
 
   const showModalByScroll = () => {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -244,7 +242,55 @@ window.addEventListener("DOMContentLoaded", () => {
     console.log(document.documentElement.scrollHeight); // изм-ие h контента, вкл-ая содержимое, нев-ое из-за прокрутки
   };
 
-  window.addEventListener("scroll", showModalByScroll);
+  window.addEventListener("scroll", showModalByScroll); //####################### ИСПОЛЬЗУЕМ КЛАССЫ ДЛЯ КАРТОЧЕК ############################
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 27;
+      this.chahgeYoUAH();
+    }
+
+    chahgeYoUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+            <img src=${this.src} alt=${this.alt} />
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">
+              ${this.descr}
+            </div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+            </div>
+          `;
+      this.parent.append(element);
+    }
+
+  }
+
+  new MenuCard("img/tabs/vegy.jpg", "vegy", 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, ".menu .container").render();
+  new MenuCard("img/tabs/elite.jpg", "elite", "Меню “Премиум”", "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты,фрукты - ресторанное меню без похода в ресторан!", 15, ".menu .container", "menu__item", "big").render();
+  new MenuCard("img/tabs/post.jpg", "post", 'Меню "Постное"', "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.", 12, ".menu .container", "menu__item", "big").render();
 });
 
 /***/ })

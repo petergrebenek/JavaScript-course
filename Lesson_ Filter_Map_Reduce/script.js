25876ded99f7a7bc47b4993a71fb2126af0be17b
Peter Grebenek <petergrebenek@gmail.com>
2022-08-26 10:16:03 +0300

    add example for lesson filter, map, reduce

--- Lesson_ Filter_Map_Reduce/script.js
+++ Lesson_ Filter_Map_Reduce/script.js
@@ -1,67 +1,184 @@
 "use strict";
 
-// console.log("Запрос данных...");
+//================================ FILTER ==================================
 
-// // Создание промиса:
-// // resolve - что-то выполнилось правильно;
-// // reject - обещание не выполнилось
+/*
+const names = ["Ivan", "Ann", "Ksenia", "Voldemart"];
 
-// const req = new Promise(function (resolve, reject) {
-//   setTimeout(() => {
-//     console.log("Подготовка данных...");
+const shortNames = names.filter((name) => {
+  return name.length < 5;
+});
 
-//     const product = {
-//       name: "TV",
-//       price: 2000,
-//     };
+console.log(shortNames);
+*/
 
-//     resolve(product);
-//   }, 2000);
-// });
+//================================= MAP ===================================
 
-// //.then() - выполняется в случаем успешного выполнения промиса resolve
+/*
+let answers = ["IvAn", "AnnA", "Hello"];
 
-// req.then((product) => {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       product.status = "order";
-//       // reject();
-//       resolve(product);
-//     }, 2000);
-//   })
-//     .then((data) => {
-//       // как-то он понимает. что нужно взять именно product нужно подставить вместо data
-//       data.modify = true;
-//       return data;
-//     })
-//     .then((data) => {
-//       console.log(data);
-//     })
-//     .catch(() => {
-//       // выполняется в случае, когда что-то пошло не так reject
-//       console.error("Произошла ошибка");
-//     })
-//     .finally(() => {
-//       //выполняется в любом случае выполнения resolve or reject
-//       console.log("Finally");
-//     });
-// });
+const result = answers.map((item) => item.toLocaleLowerCase());
 
-const test = (time) => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(), time);
-  });
+console.log(result);
+*/
+
+//================================ SOME ===================================
+
+//Eсли хоть один элемент массива подойдут по условию то вернёт true
+
+/*
+const some = [4, "qwq", "sdfsgsdg"];
+
+console.log(some.some((item) => typeof (item)  === "number")); //true
+*/
+
+//================================ EVERY ==================================
+
+//Eсли все элементы массива подойдут по условию то вернёт true
+
+/*
+const every = [4, "qwq", "sdfsgsdg"];
+const every1 = [5, 6, 7, 8];
+
+console.log(every.every((item) => typeof item === "number")); // false
+console.log(every1.every((item) => typeof item === "number")); // true
+*/
+
+//================================ REDUCE ==================================
+/*
+const arr = [3, 5, 1, 3, 2, 6];
+
+// sum - накопитель
+// current - текущий элемент массива arr
+const res = arr.reduce((sum, current) => sum + current, 3); // 3 начально значение sum
+console.log(res);
+*/
+// Для строк
+// const arr = ["apple", "pear", "plum"];
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+// console.log(res);
+
+//------------------------------ПРИМЕР1------------------------------
+/*
+const obj = {
+  ivan: "persone",
+  ann: "persone",
+  dog: "animal",
+  cat: "animal",
 };
 
-// test(1000).then(() => console.log("1000ms"));
-// test(2000).then(() => console.log("2000ms"));
+const newArr = Object.entries(obj) //сформировать массив из объекта
+  .filter((item) => item[1] === "persone")
+  .map((item) => item[0]);
 
-// Promise.all([test(1000), test(2000)]).then(() => {
-//   // ожидает окнчания выполнения всех промисов
-//   console.log("ALL");
-// });
+console.log(newArr);
+*/
 
-Promise.race([test(1000), test(2000)]).then(() => {
-  // ожидает окнчания выполнения какого-то одного промиса
-  console.log("ALL");
-});
+//_______________________________ПРИМЕР2__________________________________
+
+// У вас есть список фильмов с рейтингом в виде массива объектов.
+// Напишите функцию showGoodFilms, которая будет принимать этот массив,
+// а возвращать будет массив объектов только с теми фильмами, у которых рейтинг больше или равен 8.
+
+const films = [
+  {
+    name: "Titanic",
+    rating: 9,
+  },
+  {
+    name: "Die hard 5",
+    rating: 5,
+  },
+  {
+    name: "Matrix",
+    rating: 8,
+  },
+  {
+    name: "Some bad film",
+    rating: 4,
+  },
+];
+
+/**
+ *
+ * @param {Array} arr
+ */
+/**
+function showGoodFilms(arr) {
+  const newArr = arr.filter((item) => item.rating >= 8);
+  return newArr;
+}
+
+console.log(showGoodFilms(films));
+*/
+
+//_______________________________ПРИМЕР3__________________________________
+
+// Напишите функцию showListOfFilms, которая будет принимать этот же массив,
+//  возвращать будет строку, которая содержит названия фильмов через запятую.
+/*
+function showListOfFilms(arr) {
+  return arr.reduce(
+    (acc, curr) => `${typeof acc === "object" ? acc.name : acc}, ${curr.name}`
+  );
+  //   const newArr1 = films.map((item) => item.name).join(", ");
+  //   return newArr1;
+}
+
+console.log(showListOfFilms(films));
+*/
+//_______________________________ПРИМЕР4__________________________________
+
+// Напишите функцию setFilmsIds, которая будет принимать этот же массив,
+// а возвращать будет такой же массив с фильмами, но у каждого фильма будет новое поле id.
+// Значение этого поля установите по нумерации фильма.
+/**
+function setFilmsIds(arr) {
+  const newArr2 = arr.map((item, index) => {
+    item.id = index;
+  });
+  return arr;
+}
+
+console.log(setFilmsIds(films));
+
+//_______________________________ПРИМЕР5__________________________________
+
+// Запишите результат предыдущей функции в переменную tranformedArray.
+// Напишите функцию checkFilms, которая будет проверять, что в каждом из фильмов есть поле id.
+// Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться,
+// если мы передаем checkFilms(tranformedArray);
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+  return arr.every((item) => item.hasOwnProperty("id"));
+}
+
+console.log(checkFilms(tranformedArray));
+*/
+
+//_______________________________ПРИМЕР5__________________________________
+
+//У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount,
+//которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
+
+const funds = [
+  { amount: -1400 },
+  { amount: 2400 },
+  { amount: -1000 },
+  { amount: 500 },
+  { amount: 10400 },
+  { amount: -11400 },
+];
+
+const getPositiveIncomeAmount = (data) => {
+  let initValue = 0;
+  return data.reduce(
+    (sum, current) => sum + (current.amount > 0 ? current.amount : 0),
+    initValue
+  );
+  // return data.filter(item => item.amount > 0).reduce((acc, curr) => acc + curr.amount, 0);
+};
+
+console.log(getPositiveIncomeAmount(funds));

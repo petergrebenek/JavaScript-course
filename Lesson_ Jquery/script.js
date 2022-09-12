f68d9ac144ae1921091fc2b0be27a7c0b4655e65
Peter Grebenek <petergrebenek@gmail.com>
2022-09-12 07:35:18 +0300

    feat: add class active first button

--- Lesson_ Jquery/script.js
+++ Lesson_ Jquery/script.js
@@ -1,5 +1,15 @@
 import $ from "jquery";
 
-const btn = $("#btn"); //получение элемента
+// const btn = $("#btn"); //получение элемента
 
-console.log(btn);
+// console.log(btn);
+
+$(document).ready(function () { // Добавить класс active к первой кнопке
+    $('.list-item:first').hover(function() {
+        $(this).toggleClass('active');
+    });
+    
+    $('.list-item:eq(2)').on('click', function() { // скрыть все четные элементы
+        $('.image:even').fadeToggle();
+    });
+});
